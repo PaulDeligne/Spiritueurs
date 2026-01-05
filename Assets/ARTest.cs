e668998e4a77d3a97e3c206f5a7b250fce480ccb
PaulDeligne <paul.deligne@etu.umontpellier.fr>
2026-01-05 15:23:56 +0100

    ça se lance sur mon tel

---
+++ Assets/ARTest.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class ARTest : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("AR started");
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
